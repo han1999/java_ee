27fb1cb8d95b11730af440c1a268bdf42b8a3cfc
Han Xiao <1043599451@qq.com>
2022-04-07 18:29:42 +0800

    Use servlet in IDEA(3 ways)

---
+++ servlet/src/com/hanxiao/ThirdServlet.java
@@ -0,0 +1,28 @@
+package com.hanxiao;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @description:
+ * @author: Han Xiao
+ * @date: 2022/4/7
+ **/
+@WebServlet("/servlet3")
+public class ThirdServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp);
+        System.out.println("get third servlet!");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doPost(req, resp);
+        System.out.println("post third servlet!");
+    }
+}
