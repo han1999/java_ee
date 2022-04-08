e9c6e8492e5a4adca2864149bf3114ec9599e5dc
Han Xiao <1043599451@qq.com>
2022-04-08 16:43:53 +0800

    lifeCycle, url-patterns, priority, special url-patterns

---
+++ servlet/src/com/hanxiao/priority/Servlet2.java
@@ -0,0 +1,18 @@
+package com.hanxiao.priority;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//@WebServlet(name = "Servlet2",urlPatterns = "/*")
+public class Servlet2 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().println("/*");
+    }
+}
